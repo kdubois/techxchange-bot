9521700428d49197c7b62dcf19db073b04700bf8
Natale Vinto <nvinto@redhat.com>
2024-10-20 07:15:30 +0200

    Add RAG support with pgvector

---
+++ src/main/java/com/redhat/RagRetriever.java
@@ -0,0 +1,32 @@
+package com.redhat;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.content.injector.ContentInjector;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+
+public class RagRetriever {
+
+    @Produces
+    @ApplicationScoped
+    public RetrievalAugmentor create(EmbeddingStore store, EmbeddingModel model) {
+        var contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingModel(model)
+                .embeddingStore(store)
+                .maxResults(3)
+                .build();
+
+        return DefaultRetrievalAugmentor.builder()
+                .contentRetriever(contentRetriever)
+                .build();
+    }
+}
